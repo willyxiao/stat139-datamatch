9fc4ae91239605609699ff6d81c4066748903cd3
Willy Xiao <wxiao@hcs.harvard.edu>
2015-04-25 21:09:34 -0400

    change load_data

--- load_data.R
+++ load_data.R
@@ -1,7 +1,7 @@
-pairs = read.csv('pairs.csv', col.names=c('u1','u2'))
+pairs = read.csv('data/pairs.csv', col.names=c('u1','u2'))
 pairs = pairs[pairs$u2 != 0,]
 
-users = read.csv('user_data.csv', 
+users = read.csv('data/user_data.csv', 
                  col.names=c('id', 
                              'dorm_id', 
                              'class_year_id', 
@@ -10,7 +10,7 @@ users = read.csv('user_data.csv',
                              'created_at', 
                              'dating'))
 
-prior.willingness = read.csv('prior_willingness.csv',
+prior.willingness = read.csv('data/prior_willingness.csv',
                              col.names=c('user_id', 'answer_id'))
-posterior.willingness = read.csv('posterior_willingness.csv',
+posterior.willingness = read.csv('data/posterior_willingness.csv',
                                  col.names=c('u1_id', 'u2_id', 'u1_willing', 'u2_willing'))
