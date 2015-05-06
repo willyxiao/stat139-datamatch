9109dfb89f49f60b1327df4dfd7723a4abab645f
Kevin Eskici <keskici@college.harvard.edu>
2015-05-06 14:15:19 -0400

    new best model without correlated terms

--- build_model_best.R
+++ build_model_best.R
@@ -1,13 +1,5 @@
 source('config.R')
 
-base.model = glm(Y~.-id-age-house-oncampus,
-                 family=binomial(),
-                 data=master.frame)
-
-best.model = step(base.model,
-                  scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
-                             upper=glm(Y~(.-id-age-house-oncampus)^2,family=binomial(),data=master.frame)),
-                  direction="both")
 #Let's get rid of our variables with high correlation (kinky, creative, and twisted were highly 
 #correlated with social)
 base.model = glm(Y~.-id-age-house-oncampus-kinky-creative-twisted,
@@ -18,14 +10,14 @@ best.model = step(base.model,
                   scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
                              upper=glm(Y~(.-id-age-house-oncampus-kinky-creative-twisted)^2,family=binomial(),data=master.frame)),
                   direction="both")
-model.base.BIC = step(base.model2,
+model.base.BIC = step(base.model,
                       scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
                                  upper=glm(Y~(.-id-age-house-oncampus-kinky-creative-twisted),family=binomial(),data=master.frame)),
                       direction="both",
                       k=log(nrow(master.frame)))
 
 
-base.model.AIC_no_interactions = step(base.model2,
+base.model.AIC_no_interactions = step(base.model,
                       scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
                                  upper=glm(Y~(.-id-age-house-oncampus-kinky-creative-twisted),family=binomial(),data=master.frame)),
                       direction="both")
@@ -34,6 +26,6 @@ model.base.AIC = step(base.model.AIC_no_interactions,
                       scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
                                  upper=glm(Y~(.-id-age-house-oncampus-kinky-creative-twisted)^2,family=binomial(),data=master.frame)),
                       direction="both")
-
 save(model.base.BIC, file="data/model_base_BIC.RData")
 save(model.base.AIC, file="data/model_base_AIC.RData")
+save(model.best, file="data/model_best.RData")
