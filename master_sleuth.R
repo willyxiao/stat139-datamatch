2a85e5970a44d598e178ef9d1b6a72f05299edf6
Kevin Eskici <keskici@college.harvard.edu>
2015-05-06 12:31:52 -0400

    added BIC to master_sleuth

--- master_sleuth.R
+++ master_sleuth.R
@@ -8,3 +8,26 @@ best.model = step(base.model,
                   scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
                              upper=glm(Y~(.-id-age-house-oncampus)^2,family=binomial(),data=master.frame)),
                   direction="both")
+
+base.model.AIC = step(base.model,
+                      scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
+                                 upper=glm(Y~(.-id-age-house-oncampus),family=binomial(),data=master.frame)),
+                      direction="both")
+
+best.model.AIC = step(base.model.AIC,
+                      scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
+                                 upper=glm(Y~(.-id-age-house-oncampus)^2,family=binomial(),data=master.frame)),
+                      direction="both")
+
+base.model.BIC = step(base.model,
+                  scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
+                             upper=glm(Y~(.-id-age-house-oncampus),family=binomial(),data=master.frame)),
+                  direction="both",
+                  k=log(nrow(master.frame)))
+
+best.model.BIC = step(base.model.BIC,
+                      scope=list(lower=glm(Y~1,family=binomial(),data=master.frame),
+                                 upper=glm(Y~(.-id-age-house-oncampus)^2,family=binomial(),data=master.frame)),
+                      direction="both",
+                      k=log(nrow(master.frame)))
+
